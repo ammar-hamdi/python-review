b3fafd002b81e9615e923e24c3c7f8938f34aa3c
ammar-hamdi <48071356+ammar-hamdi@users.noreply.github.com>
2026-01-18 12:58:30 +0100

    Create day1_basics.py

---
+++ day1_basics.py
@@ -0,0 +1,28 @@
+# Day 1 - Python Basics
+
+name = "Ammar"
+age = 28
+height = 1.75
+is_student = False
+
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My height is {height}")
+print(f"Student? {is_student}")
+
+# Simple operations
+a = 10
+b = 3
+print("a + b =", a + b)
+print("a / b =", a / b)
+print("a * b =", a * b)
+
+# Mini Project: Grades Analyzer
+students = ["Ahmed", "Sara", "Omar"]
+grades = [85, 90, 78]
+
+average = sum(grades) / len(grades)
+
+print("\nStudents:", students)
+print("Grades:", grades)
+print("Average grade:", average)
